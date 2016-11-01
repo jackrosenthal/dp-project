174453ef664ce110dc8ab9cad9b523d9a143ed9b
Jack Rosenthal <jack@rosenth.al>
2016-11-01 15:18:45 -0600

    13 lines

--- dp.py
+++ dp.py
@@ -9,6 +9,5 @@ def optimal_events(ev):
         return evtuple(t, pos) + max(
                 tuple(mevents(t + 1, p)
                     for p in range(pos - 1, pos + 2) if len(ev) - t >= abs(p - ev[-1])
-                ),
-                key=len)
+                ), key=len)
     return mevents(0, 0)
