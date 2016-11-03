a8ddb9af2b6b7f919a5ca47de46ba226220e4f33
Jack Rosenthal <jack@rosenth.al>
2016-11-03 13:40:57 -0600

    9 lines

--- dp.py
+++ dp.py
@@ -1,13 +1,9 @@
 from functools import lru_cache
 def optimal_events(ev):
-    def evtuple(t, pos):
-        return (t,) if t >= 1 and ev[t-1] == pos else ()
     @lru_cache(maxsize=None)
     def mevents(t, pos):
-        if t == len(ev):
-            return evtuple(t, pos)
-        return evtuple(t, pos) + max(
+        return ((t,) if t >= 1 and ev[t-1] == pos else ()) + (max(
                 tuple(mevents(t + 1, p)
                     for p in range(pos - 1, pos + 2) if len(ev) - t >= abs(p - ev[-1])
-                ), key=len)
+                ), key=len) if t != len(ev) else ())
     return mevents(0, 0)
